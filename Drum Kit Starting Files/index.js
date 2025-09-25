1897d359c89d423cf5ccb8283d9ca68a850b507b
Gustavo Neves <gustavohenrique19022@gmail.com>
2025-09-25 16:22:20 -0300

    JQuery

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -6,12 +6,15 @@ for(var i = 0; i<numberOfDrumButtons; i++){
         var buttonInnerHTML = this.innerHTML;
 
         makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
     })
 }
 
 
 document.addEventListener("keypress", function(event){
     makeSound(event.key);
+    buttonAnimation(event.key);
 });
 
 function makeSound(key){
@@ -48,5 +51,18 @@ function makeSound(key){
 
             default: console.log(buttonInnerHTML);
         }
+}
+
+function buttonAnimation(currentKey){
+
+    var activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+
+    },100);
+
 }
 
