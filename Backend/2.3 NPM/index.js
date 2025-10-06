dbab72c7f1e06af1975825f3137773826862d915
Gustavo <gustavohenrique19022@gmail.com>
2025-10-06 12:58:32 -0300

    att

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,12 @@
+//var generateName = require("sillyname");
+
+// import generateName from "sillyName"
+import superheores, { randomSuperhero } from "superheroes"
+
+// var sillyName = generateName();
+
+var superHeroName = randomSuperhero();
+
+console.log(`My superhero's name is ${superHeroName}.`);
+
+// console.log(`My name is ${sillyName}.`);
